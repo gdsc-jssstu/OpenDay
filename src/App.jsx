07bc191d6ea86e4290085c7e3c03b7140eed0833
vikhyat-gowda <vikhyat.gowda1@gmail.com>
2023-05-18 21:14:35 +0530

    Added department details route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import { useState, useEffect } from 'react';
-import { SignUp, Login, Homepage } from './pages';
-import {Routes, Route} from 'react-router-dom';
-import "./App.css"
-import Feedback from './pages/Feedback';
+import { useState, useEffect } from "react";
+import { SignUp, Login, Homepage } from "./pages";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Feedback from "./pages/Feedback";
+import DepartmentDetails from "./pages/DeptDetails";
 
 const App = () => {
+  const [token, setToken] = useState(false);
 
- const [token, setToken] = useState(false)
-
-  if(token){
-    sessionStorage.setItem('token',JSON.stringify(token))
+  if (token) {
+    sessionStorage.setItem("token", JSON.stringify(token));
   }
 
   useEffect(() => {
-    if(sessionStorage.getItem('token')){
-      let data = JSON.parse(sessionStorage.getItem('token'))
-      setToken(data)
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setToken(data);
     }
-    
-  }, [])
-  
+  }, []);
 
- 
   return (
     <div>
       <Routes>
-        <Route path={'/signup'} element={ <SignUp />} />
-        <Route path={'/'} element={ <Login setToken={setToken}/>} />
-        {token?<Route path={'/homepage'} element={ <Homepage token={token} />} />:""}
-        {token?<Route path={'/feedback'} element={<Feedback token={token} />} /> : ""}
-      </Routes>
-     
-      
-    </div>
-  )
-}
+        <Route path={"/signup"} element={<SignUp />} />
+        <Route path={"/"} element={<Login setToken={setToken} />} />
+        <Route path={"/dept/:name"} element={<DepartmentDetails />} />
 
-export default App
+        {!token ? (
+          <Route path={"/homepage"} element={<Homepage token={token} />} />
+        ) : (
+          ""
+        )}
+        {token ? (
+          <Route path={"/feedback"} element={<Feedback token={token} />} />
+        ) : (
+          ""
+        )}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
