792e1c32113035da8ed62c6e1ff9bb1ccd12a223
vikhyat-gowda <vikhyat.gowda1@gmail.com>
2023-05-18 21:14:07 +0530

    added image carousel

--- src/components/DeptSection.jsx
+++ src/components/DeptSection.jsx
@@ -1,26 +1,26 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
-const DeptSection = ({
-  heading,
-  content,
-  isList = false,
-  subContent,
-  images,
-}) => {
+const DeptSection = ({ heading, content, isList = false, images }) => {
   return (
     <article className="tracking-wide">
       <h4 className="my-4 text-white text-2xl font-semibold ">{heading}</h4>
 
       {images && (
-        <Carousel showThumbs={false}>
-          {images.map((imagePath) => (
-            <img src={imagePath} />
-          ))}
-        </Carousel>
+        <div className="flex justify-center">
+          <Carousel showThumbs={false} className="mb-4  max-w-3xl">
+            {images.map((imagePath, i) => (
+              /**
+               * key is set to index, since the array is never changed or reordered
+               * ref:
+               * https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
+               */
+              <img src={imagePath} key={i} />
+            ))}
+          </Carousel>
+        </div>
       )}
-
-      <section className="p-6 bg-secondary rounded-lg">
+      <section className="p-6  bg-secondary rounded-lg">
         {!isList ? (
           <p>{content}</p>
         ) : (
@@ -28,7 +28,8 @@ const DeptSection = ({
             {content.map((ele, i) => (
               /**
                * key is set to index, since the array is never changed or reordered
-               * ref: https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
+               * ref:
+               * https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
                */
               <li key={i}>{ele}</li>
             ))}
