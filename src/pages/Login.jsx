4554ef3cc3857ee2d2057cd49a33ce1134c2d0f7
vikhyat-gowda <vikhyat.gowda1@gmail.com>
2023-05-28 12:43:43 +0530

    changed redirect path to routemap page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,91 +1,74 @@
-import { useState } from 'react';
-import { Link, useNavigate} from 'react-router-dom';
-import { supabase } from '../client';
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { supabase } from "../client";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import AuthIcon from "../assets/AuthIcon.svg"
+import AuthIcon from "../assets/AuthIcon.svg";
 
-import "../assets/css/auth.css"
+import "../assets/css/auth.css";
 
-const Login = ({setToken}) => {
-  let navigate = useNavigate()
-  const alertDiv = document.getElementById('invalid')
-  const [formData,setFormData] = useState({
-        email:'',password:''
-  })
+const Login = ({ setToken }) => {
+  let navigate = useNavigate();
+  const alertDiv = document.getElementById("invalid");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-
-  function handleChange(event){
-    setFormData((prevFormData)=>{
-      return{
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
         ...prevFormData,
-        [event.target.name]:event.target.value
-      }
-
-    })
-
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
-  async function handleSubmit(e){
-    e.preventDefault()
+  async function handleSubmit(e) {
+    e.preventDefault();
 
     try {
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: formData.email,
-            password: formData.password,
-          })
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: formData.email,
+        password: formData.password,
+      });
 
-      if (error) throw error
-      setToken(data)
-      navigate('/homepage')
+      if (error) throw error;
+      setToken(data);
+      navigate("/routemap");
 
-
-    //   alert('Check your email for verification link')
-
-      
+      //   alert('Check your email for verification link')
     } catch (error) {
-
       alert(error);
     }
   }
 
-
-
-
   return (
-    <div className='login-main' >
-        <img src={AuthIcon} alt="authIcon" className='authIcon' />
-        <div className="info">
-          <h3>OPEN DAY'23</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adispicing elit</p>
-        </div>
+    <div className="login-main">
+      <img src={AuthIcon} alt="authIcon" className="authIcon" />
+      <div className="info">
+        <h3>OPEN DAY'23</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adispicing elit</p>
+      </div>
       <form onSubmit={handleSubmit}>
-        <input 
-          placeholder='Email'
-          name='email'
-          onChange={handleChange}
-        />
+        <input placeholder="Email" name="email" onChange={handleChange} />
         <br />
-        <input 
-          placeholder='Password'
-          name='password'
+        <input
+          placeholder="Password"
+          name="password"
           type="password"
           onChange={handleChange}
         />
         <br />
-        <button type='submit'>
-          Sign In
-        </button>
+        <button type="submit">Sign In</button>
         <br />
-
       </form>
-      <p className='sign-up-link'>
-      Dont have an account? <Link to='/signup'>Sign Up</Link> 
+      <p className="sign-up-link">
+        Dont have an account? <Link to="/signup">Sign Up</Link>
       </p>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
