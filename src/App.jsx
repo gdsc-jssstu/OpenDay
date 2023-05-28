a9bc0a18359e30bb823f04e827166d2d3838cb13
vikhyat-gowda <vikhyat.gowda1@gmail.com>
2023-05-28 12:42:05 +0530

    changed dept details component name

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import {
 } from "./pages";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import Feedback from "./pages/Feedback";
-import DepartmentDetails from "./pages/DeptDetails";
-import RouteMap from "./pages/RouteMap";
 
 const App = () => {
   const [token, setToken] = useState(false);
@@ -32,7 +29,7 @@ const App = () => {
       <Routes>
         <Route path={"/signup"} element={<SignUp />} />
         <Route path={"/"} element={<Login setToken={setToken} />} />
-        <Route path={"/dept/:name"} element={<DepartmentDetails />} />
+        <Route path={"/dept/:name"} element={<DeptDetails />} />
         <Route path={"/routemap"} element={<RouteMap />} />
         <Route path={"/homepage"} element={<Homepage token={token} />} />
         <Route path={"/feedback"} element={<Feedback token={token} />} />
